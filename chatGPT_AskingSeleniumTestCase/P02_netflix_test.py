a69802011057fc09e3de7a99bf189d4e4cd8b142
Berke <93223660+quatronostro@users.noreply.github.com>
2023-07-05 20:46:48 +0300

    netflix temp

---
+++ chatGPT_AskingSeleniumTestCase/P02_netflix_test.py
@@ -0,0 +1,34 @@
+
+
+
+# Test Case 2:
+# Website: www.netflix.com
+# Objective: Sign up for a new account and perform a search for a TV show.
+
+# Steps:
+
+# Launch the browser and navigate to www.netflix.com.
+def launch_browser_navigate_website(url):
+    pass
+
+# Click on the "Sign In" button.
+# Select the "Sign Up" option.
+def sign_up_action():
+    pass
+
+# Enter valid information details, including name, email, and password.
+def enter_valid_info():
+    pass
+
+# Access your profile and verify that the user is logged in.
+def verify_login():
+    pass
+
+# Perform a search for a specific TV show.
+def search():
+    pass
+
+# Verify that the search results display the TV show and relevant information.
+def verify_search_result():
+    pass
+
