0e83b7dc5760c4d06731d9a0d7189deddb72e769
Berke <93223660+quatronostro@users.noreply.github.com>
2023-07-20 16:41:50 +0300

    netflix1

--- chatGPT_AskingSeleniumTestCase/P02_netflix_test.py
+++ chatGPT_AskingSeleniumTestCase/P02_netflix_test.py
@@ -1,4 +1,5 @@
-
+from selenium.webdriver.common.by import By
+from selenium import webdriver
 
 
 # Test Case 2:
@@ -9,12 +10,20 @@
 
 # Launch the browser and navigate to www.netflix.com.
 def launch_browser_navigate_website(url):
-    pass
+    driver.get(url)
 
 # Click on the "Sign In" button.
 # Select the "Sign Up" option.
 def sign_up_action():
-    pass
+    driver.find_element(By.XPATH, "//button[text()='Accept']").click() #Cookies Accept button
+    driver.find_element(By.XPATH, "//a[text()='Sign In']").click() #Sign In button
+
+    emailBox = driver.find_element(By.XPATH, "//*[@for='id_userLoginId']")
+    emailBox.send_keys("mbaramuk@hotmail.com")
+    passwordBox = driver.find_element(By.XPATH, "//*[@for='id_password']")
+    passwordBox.send_keys("Muz994599 ")
+
+    driver.find_element(By.XPATH, "//*[@type='submit']").click() #submit button
 
 # Enter valid information details, including name, email, and password.
 def enter_valid_info():
@@ -32,3 +41,13 @@ def search():
 def verify_search_result():
     pass
 
+
+if __name__ == '__main__':
+    url = "https://www.netflix.com"
+
+    driver = webdriver.Chrome()
+    driver.implicitly_wait(10)
+    driver.maximize_window()
+
+    launch_browser_navigate_website(url)
+    sign_up_action()
